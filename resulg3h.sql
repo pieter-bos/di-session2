34402609e8b8498b957d7cd57e28de4d5496a272
Pieter Bos <info@pieterbos.me>
2014-05-06 17:33:03 +0200

    Working 2.3h

--- resulg3h.sql
+++ resulg3h.sql
@@ -1,7 +1,6 @@
-select min(person.name), count(person.name) from person, directs, acts where
+select min(person.name), count(distinct(acts.mid)) from person, directs, acts where
     directs.pid = person.pid
     and acts.pid = person.pid
-    and acts.mid = directs.mid
 group by acts.pid
 order by count(acts.mid) desc
 limit 3;
