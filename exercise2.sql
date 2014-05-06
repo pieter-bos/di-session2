d8d290e93fa65bfb99f11da0e898476afab49ea7
Pieter Bos <info@pieterbos.me>
2014-05-06 15:29:00 +0200

    Added 2.2b

--- exercise2.sql
+++ exercise2.sql
@@ -13,3 +13,29 @@ create table f (
     violation boolean,
     motivation varchar(200)
 );
+
+create table r1 (
+    d int,
+    e int
+);
+
+create table f1 (
+    x varchar(10),
+    y varchar(10),
+    violation boolean,
+    motivation varchar(200)
+);
+
+create table r2 (
+    a int,
+    b int,
+    c int,
+    f int
+);
+
+create table f2 (
+    x varchar(10),
+    y varchar(10),
+    violation boolean,
+    motivation varchar(200)
+)
--- result2.sql
+++ result2.sql
@@ -2,3 +2,10 @@ insert into f (x, y, violation, motivation) values
     ('abc', 'ef', false, 'superkey'),
     ('e', 'd', true, 'leidt slechts tot ADE'),
     ('d', 'a', true, 'leidt alleen tot DA, niet ABCDEF');
+
+insert into f1 (x, y, violation, motivation) values
+    ('e', 'd', false, 'superkey');
+
+insert into f2 (X, y, violation, motivation) values
+    ('abc', 'ef', false, 'superkey'),
+    ('e', 'a', true, 'leidt slecht tot AE');
