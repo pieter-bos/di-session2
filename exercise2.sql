2a4d6c95c8fbe9a918650518304632f1bb893d0c
Pieter Bos <info@pieterbos.me>
2014-05-06 15:24:47 +0200

    2.2a

---
+++ exercise2.sql
@@ -0,0 +1,15 @@
+create table r (
+    a int,
+    b int,
+    c int,
+    d int,
+    e int,
+    f int
+);
+
+create table f (
+    x varchar(10),
+    y varchar(10),
+    violation boolean,
+    motivation varchar(200)
+);
---
+++ result2.sql
@@ -0,0 +1,4 @@
+insert into f (x, y, violation, motivation) values
+    ('abc', 'ef', false, 'superkey'),
+    ('e', 'd', true, 'leidt slechts tot ADE'),
+    ('d', 'a', true, 'leidt alleen tot DA, niet ABCDEF');
